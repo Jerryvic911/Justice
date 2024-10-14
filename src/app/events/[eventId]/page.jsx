eea253d6ef8b3066dc20c059786e0b5ed8433dda
jeremiahv <jeremiahv791@gmail.com>
2024-10-14 11:19:40 +0100

    started working on the events preview page

--- src/app/events/[eventId]/page.jsx
+++ src/app/events/[eventId]/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState } from 'react';
 import eventData from '../../../data/events.data.json';
-import Donation from '../../donate/Donation';
 
 export default function EventDetails({ params }) {
   const [event] = useState(() => {
@@ -9,16 +8,56 @@ export default function EventDetails({ params }) {
   });
 
   return (
-    <main className='h-fit p-8'>
-      <section>
-        <h1 className='mb-6 text-3xl font-bold text-gray-800'>{event.title}</h1>
-
-        <p className='tracking-wider'>
-          {event.description} <br />
+    <main className='flex flex-col mt-4 mb-6 px-4 md:px-6 lg:px-8 xl:px-[10rem]'>
+      <div className='w-full mb-6'>
+        <h1 className='text-[24px] md:text-[28px] lg:text-[32px] font-semibold mb-2'>Event Details</h1>
+        <h2 className='text-[20px] md:text-[24px] lg:text-[28px] font-semibold'>{event.title}</h2>
+      </div>
+      <section className='w-full'>
+        <div className='mb-4 text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-t border-black pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Date:</span>
+          <span>{event.date}</span>
+        </div>
+        <div className='mb-4 text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-t border-black pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Time:</span>
+          <span>{event.time}</span>
+          <span className='mx-2'>-</span>
+          <span>{event.endTime}</span>
+        </div>
+        <div className='mb-4 text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-t border-black pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Location:</span>
+          <span>{event.location}</span>
+        </div>
+        <p className='border-t text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-black mb-4 pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Description:</span>
+          <span>{event.description}</span>
         </p>
+        <div className='mb-4 text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-t border-black pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Topics:</span>
+          <span>{event.topics.join(' - ')}</span>
+        </div>
+        <div className='mb-4 text-[14px] md:text-[16px] lg:text-[18px] font-[500] border-t border-b pb-4 border-black pt-5 flex flex-col md:flex-row md:items-center'>
+          <span className='text-[#707070] font-[600] md:mr-4'>Speakers/Trainers:</span>
+          <div className='flex flex-col'>
+            <span>{event.speakers}</span>
+            <span>{event.speakers1}</span>
+          </div>
+        </div>
       </section>
-      <div>
-        <Donation />
+
+      <div className='mt-6'>
+        <div>
+          <h2 className='text-lg md:text-xl font-semibold'>Registration Information:</h2>
+        </div>
+        <button className='w-[150px] h-[40px] mt-3 px-6 py-2 rounded-full text-white bg-[#423F3F]'>Register Now</button>
+        <div className='mt-5'>
+          <p className='text-[14px] md:text-[16px] font-[500] mb-4'>
+            Contact for Questions: 
+            <span className='ml-1'>
+              <a href="mailto:contact@kirki.com" className='text-blue-500'>contact@kirki.com</a>
+            </span>
+          </p>
+        </div>
       </div>
     </main>
   );
